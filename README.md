b7467bf05f518a7aeb091b08531a6116ddb2ca34
Kauzi Joshi <kaustubhjoshi1411@gmail.com>
2018-01-16 15:32:16 +0530

    Stored the data in numpy and changed read me to add the source

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BelgianTS
 Tensorflow traffic sign detection
 
-The code is implementation of the Tensorflow tutorial with example of Belgian Traffic Sign detection.
+The code is implementation of the Tensorflow tutorial with example of Belgian Traffic Sign detection given by 
+www.datacamp.com with some minorchanges.
 
 The code is written in python2.7
--- prepDataset.py
+++ prepDataset.py
@@ -22,12 +22,20 @@ class ClassifySigns:
 		Function used to visualize images, checked that the size of images are different, there will be a need to compress them and convert
 		them to a constant size.
 	"""
-	def visualizeImages(self, la):
+	def visualizeImages(self, la, typ):
 		for i in range(0,len(la)):
 			plt.subplots()
 			plt.axis('off')
-#			plt.imshow(self.images[la[i]])
-			plt.imshow(self.images[la[i]], cmap="gray")
+			"""
+				Use this code for rgb
+			"""
+			if typ == 'rgb':
+				plt.imshow(self.images[la[i]])
+			"""
+				Use the code below when observing gray images
+			"""
+			if typ == 'gray':
+				plt.imshow(self.images[la[i]], cmap="gray")
 			plt.subplots_adjust(wspace=0.5)
 		plt.show()
 	"""
@@ -52,20 +60,24 @@ class ClassifySigns:
 	"""
 	def normalize(self):
 		images28 = [transform.resize(image, (28, 28, 3)) for image in self.images]
-		images28 = np.array(images28)
 		#self.visualizeImages([250, 466, 2591, 3577, 4000])
-		images28 = color.rgb2grey(images28)
+		for i in range(0,len(images28)):
+			images28[i] = color.rgb2grey(np.array(images28[i]))
 		self.images = images28
-		
 	"""
 		Function used to store and retrive image as well as label data
+		The data is stored in bit format using numpy
 	"""
 	def storeImageData(self):
-		images = np.array(self.images).reshape(len(self.images),1)
-		labels = np.array(self.labels).reshape(len(self.images),1)
+		images = np.array(self.images)
+		labels = np.array(self.labels)
+		np.save('Images',images.astype(np.float))
+		np.save('Labels',labels.astype(np.float))
 if __name__ == "__main__":
 	bs = ClassifySigns()
 	bs.load_data('/home/kauzi/Documents/Data For ML/BelgiumSigns/Training')
 	#bs.uniqueImages()
+	#bs.visualizeImages([250, 466, 2591, 3577, 4000], typ = 'rgb')
 	bs.normalize()
-	bs.visualizeImages([250, 466, 2591, 3577, 4000])
+	#bs.visualizeImages([250, 466, 2591, 3577, 4000], typ = 'gray')
+	bs.storeImageData()
